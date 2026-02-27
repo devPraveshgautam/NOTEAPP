632162c888fb88d3b1fed1b984a98509d38e83de
Pravesh Gautam <praveshgautamofficial@gmail.com>
2026-02-27 13:56:13 +0545

    adding in local repo

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/view/home_page.dart';
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}

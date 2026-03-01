b5ad898a45e96c4e026db139e8a994f22d6487d2
Pravesh Gautam <praveshgautamofficial@gmail.com>
2026-03-01 20:00:55 +0545

    snackbar for data intered in database

---
+++ lib/helper/data_helper.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+
+class DatabaseHelper {
+  Future<sql.Database> database() async {
+    var databasePath = await sql.getDatabasesPath();
+    String path = join(databasePath, 'notes.db');
+    var database = await sql.openDatabase(path, version: 1, onCreate: (sql.Database db, version) async{
+      await db.execute(
+        'CREATE TABLE notes(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT)'
+      );
+    });
+        return database;
+
+  }
+  Future<void> insertData(String title, String description) async {
+    var db = await database();
+    var check = await db.insert('notes', {
+      'title': title,
+      'description': description,
+    });
+    if(check == 0) {
+       GetSnackBar(
+        title: 'Error',
+        message: 'Failed to insert data',
+        backgroundColor: Colors.red,
+        duration: Duration(seconds: 2),
+      );
+    } else {
+      GetSnackBar(
+        title: 'success',
+        message: 'successfully  inserted data',
+        backgroundColor: Colors.blue,
+        duration: Duration(seconds: 2),
+      );
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:note_app/view/pages/home_page.dart';
 void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
